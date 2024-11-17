7e8d6e8badad9324ac76b981704b8b5688e161d8
habiba <habiba.hisham2811@gmail.com>
2024-11-17 14:56:56 +0100

    asynchronus R/W files

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const fs = require('fs'); // module to  acsess the filesystem , fs is object that access file system functtions
 
+// Blocking, Synchronous way
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8'); // access fs.funcName
 // filepath, encoding type so that we dont get buffer so utf returns text
 
-console.log(textIn);
+// console.log(textIn);
 
-const textOut = `This is what we know about avocado: ${textIn}. \nCreated on ${Date.now()}`; // \n new line
+// const textOut = `This is what we know about avocado: ${textIn}. \nCreated on ${Date.now()}`; // \n new line
 
-fs.writeFileSync('./txt/output.txt', textOut, 'utf-8'); // write to file
+// fs.writeFileSync('./txt/output.txt', textOut, 'utf-8'); // write to file
 
-console.log('Done!');
+// console.log('Done!');
+
+// Non-Blocking, Asynchronous way
+
+// call back hell
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  if (err) return console.log('Error');
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+        console.log('your file has been written 😊');
+      });
+    });
+  });
+}); // after reading the file will call the callback function that has to paramtters , first one error incase of any and second one is data
+
+console.log('will read file'); // this gets logged first
