9a12a86102e59a6897a59e75abefae1c88579cf1
habiba <habiba.hisham2811@gmail.com>
2024-11-19 18:44:20 +0100

    handlers for async-await

--- index.js
+++ index.js
@@ -33,10 +33,28 @@ const getDogPic = async () => {
     console.log('All tasks completed successfully!');
   } catch (err) {
     console.log(err.message);
+    throw err;
   }
+  return '2:READY';
 };
+// 1:will get dog pics
+// Promise { <pending> }
+// 2: done getting dog pics
+// breed: retriever
+// https://images.dog.ceo/breeds/retriever-flatcoated/n02099267_1095.jpg
+// All tasks completed successfully!
+// console.log('1:will get dog pics');
+// const x = getDogPic();
+// console.log(x);
+// console.log('3: done getting dog pics');
 
-getDogPic();
+console.log('1:will get dog pics');
+getDogPic()
+  .then((x) => {
+    console.log(x);
+    console.log('3: done getting dog pics');
+  })
+  .catch((err) => console.log('   ERROR'));
 
 // Begin the promise chain by reading the 'dog.txt' file.
 // readFilePro(`${__dirname}/dog.txt`) // '__dirname' ensures the path is relative to the current directory.
