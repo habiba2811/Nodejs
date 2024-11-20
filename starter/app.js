efd139dc6e262605aacc3ed2cfb7c2e7cc41c150
habiba <habiba.hisham2811@gmail.com>
2024-11-20 13:20:19 +0100

    patch&delete methods

--- starter/app.js
+++ starter/app.js
@@ -36,6 +36,38 @@ app.get('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  if (!tour)
+    return res.status(404).json({
+      status: 'fail',
+      message: 'not found',
+    });
+  const updatedTour = { ...tour, ...req.body }; // spread operator to merge the 2 objects
+  tours[tours.findIndex((el) => el.id === id)] = updatedTour;
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: updatedTour,
+    },
+  });
+});
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'not found',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    message: 'null',
+  });
+});
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body); // the body we sent + new id created , use obj.assign to create a new obj be merging 2 existing obj ( id, req.body)
