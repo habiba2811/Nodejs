40c95bff814eb5366e1edd1442decfe583bd668a
habiba <habiba.hisham2811@gmail.com>
2024-11-21 17:35:17 +0100

    modeling tour data

--- starter/controllers/tourController.js
+++ starter/controllers/tourController.js
@@ -80,7 +80,7 @@ exports.createTour = async (req, res) => {
   } catch (error) {
     res.status(400).json({
       status: "fail",
-      message: "invalid data sent!",
+      message: error,
     });
   }
 };
--- starter/models/tourModel.js
+++ starter/models/tourModel.js
@@ -7,6 +7,26 @@ const tourSchema = new mongoose.Schema({
     required: [true, "A tour must have a name"],
     unique: true,
   },
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have a group size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have a difficulty"],
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5,
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
   rating: {
     type: Number,
     default: 4.5,
@@ -15,7 +35,29 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, "A tour must have a price"],
   },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true, // remove all white space in beggining and end
+    required: [true, "A tour must have a summary"],
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, "A tour must have a description"],
+  },
+  imageCover: {
+    type: String, // the path to the image
+    required: [true, "A tour must have a cover image"],
+  },
+  images: [String], // multiple images array of strings
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
+
 const Tour = mongoose.model("Tour", tourSchema);
 
 module.exports = Tour;
