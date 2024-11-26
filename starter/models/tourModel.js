5d9a15609aa4060996c2ad5320c7617f6f63c791
habiba <habiba.hisham2811@gmail.com>
2024-11-26 10:58:38 +0100

    data validation

--- starter/models/tourModel.js
+++ starter/models/tourModel.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const slugify = require("slugify");
+const validator = require("validator");
+
 const tourSchema = new mongoose.Schema(
   {
     // create schema to create model with documents for crud operations
@@ -7,6 +9,10 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: [true, "A tour must have a name"],
       unique: true,
+      trim: true,
+      maxlength: [40, "A tour name must have at most 40 characters"],
+      minlength: [10, "A tour name must have at least 10 characters"],
+      // validate: [validator.isAlpha, "Tour name must only contain characters"], // validator package is used to validate data input
     },
     slug: String,
     duration: {
@@ -20,10 +26,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, "A tour must have a difficulty"],
+      enum: {
+        values: ["easy", "medium", "difficult"],
+        message: "Difficulty must be either: easy, medium, difficult",
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, "Rating must be above 1.0"],
+      max: [5, "Rating must be below 5.0"],
     },
     ratingsQuantity: {
       type: Number,
@@ -37,7 +49,16 @@ const tourSchema = new mongoose.Schema(
       type: Number,
       required: [true, "A tour must have a price"],
     },
-    priceDiscount: Number,
+    priceDiscount: {
+      type: Number,
+      validate: {
+        validator: function (val) {
+          // this only points to current doc on NEW document creation, doesnt work on update
+          return val < this.price; // 100 < 200
+        },
+        message: "Discount price ({VALUE}) should be below regular price",
+      },
+    },
     summary: {
       type: String,
       trim: true, // remove all white space in beggining and end
