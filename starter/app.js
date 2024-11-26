05397069474b2c5bdbb6db387e9b663241c637bb
habiba <habiba.hisham2811@gmail.com>
2024-11-26 12:04:26 +0100

    global error handling mw

--- starter/app.js
+++ starter/app.js
@@ -13,11 +13,6 @@ if (process.env.NODE_ENV === "development") {
 
 app.use(express.json()); // express.json is middleware function that modify the incomming request data used for the post request
 app.use(express.static(`${__dirname}/public`)); // express.static for serving static html files
-app.use((req, res, next) => {
-  // it applies to all requests because the order of the code
-  console.log("hello from middleware 🙌");
-  next(); // call next to so the next middleware is called
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString(); //current time of request
@@ -28,5 +23,27 @@ app.use((req, res, next) => {
 
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
+app.all("*", (req, res, next) => {
+  // catch all route that passed the above routes and wasnt defined
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = "fail";
+  err.statusCode = 404;
+  next(err); // pass error to global error handler
+});
+
+app.use((err, req, res, next) => {
+  // global error handler
+  err.statusCode = err.statusCode || 500; // default status code
+  err.status = err.status || "error"; // default status
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
