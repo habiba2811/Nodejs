bbde76392befa060ccaa308db7d1827e4718a501
habiba <habiba.hisham2811@gmail.com>
2024-11-26 16:20:54 +0100

    catch uncaught error

--- starter/server.js
+++ starter/server.js
@@ -1,5 +1,16 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+
+process.on("uncaughtException", (err) => {
+  // event listeners
+  // catch uncaught exceptions
+  console.log("UNCAUGHT EXCEPTION! Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1); // exit with failure 1-> error , 0 -> success
+  });
+});
+
 dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
@@ -12,6 +23,19 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB).then(() => console.log("DB connection successful"));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`app listening on ${port}`);
 });
+
+// event handler for unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  // event listeners
+  // catch unhandled promise rejections
+  console.log("UNHANDLED REJECTION! Shutting down...");
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1); // exit with failure 1-> error , 0 -> success
+  });
+});
+
+console.log(x);
